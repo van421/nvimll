8e684b5afccb79831059636af04f9ea96fd92d52
van <liufan421@gmail.com>
2023-09-27 15:42:15 +0800

    format codeium's config code

--- lua/van/plugins/codeium.lua
+++ lua/van/plugins/codeium.lua
@@ -2,17 +2,21 @@ return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
 	config = function()
-		vim.g.codeium_no_map_tab = false
-		vim.keymap.set("i", "<C-enter>", function()
+		local g = vim.g
+		local keymap = vim.keymap
+
+		g.codeium_no_map_tab = false
+
+		keymap.set("i", "<C-enter>", function()
 			return vim.fn["codeium#Accept"]()
 		end, { expr = true })
-		vim.keymap.set("i", "<C-'>", function()
+		keymap.set("i", "<C-'>", function()
 			return vim.fn["codeium#CycleCompletions"](1)
 		end, { expr = true })
-		vim.keymap.set("i", "<C-;>", function()
+		keymap.set("i", "<C-;>", function()
 			return vim.fn["codeium#CycleCompletions"](-1)
 		end, { expr = true })
-		vim.keymap.set("i", "<C-x>", function()
+		keymap.set("i", "<C-x>", function()
 			return vim.fn["codeium#Clear"]()
 		end, { expr = true })
 	end,
