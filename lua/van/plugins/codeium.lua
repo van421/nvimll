09be769e69f5646c61aebfa6d6effaad068a75cf
van <liufan421@gmail.com>
2023-09-27 13:16:39 +0800

    add some keymaps to codeium

--- lua/van/plugins/codeium.lua
+++ lua/van/plugins/codeium.lua
@@ -1,4 +1,19 @@
 return {
 	"Exafunction/codeium.vim",
 	event = "BufEnter",
+	config = function()
+		vim.g.codeium_no_map_tab = false
+		vim.keymap.set("i", "<C-enter>", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true })
+		vim.keymap.set("i", "<C-'>", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true })
+		vim.keymap.set("i", "<C-;>", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true })
+		vim.keymap.set("i", "<C-x>", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true })
+	end,
 }
