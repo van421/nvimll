753796c106c1dff54a70db3f77961d7696d7ebac
van <liufan421@gmail.com>
2023-09-27 16:17:09 +0800

    update sniprun's config

--- lua/van/plugins/sniprun.lua
+++ lua/van/plugins/sniprun.lua
@@ -1,14 +1,67 @@
 return {
 	"michaelb/sniprun",
-	keys = "<leader>s",
-	run = "sh ./install.sh",
-	config = function()
+	event = "VeryLazy",
+	build = "sh ./install.sh",
+	opts = {
+		display = { "Terminal" },
+		live_display = { "VirtualTextOk", "TerminalOk" },
+		selected_interpreters = { "Python3_fifo" },
+		repl_enable = { "Python3_fifo" },
+	},
+	config = function(_, opts)
 		local sniprun = require("sniprun")
-		local display = require("sniprun.display")
-		sniprun.setup()
-		local keymap = vim.keymap
-		keymap.set("n", "<leader>sr", sniprun.run, {})
-		keymap.set("n", "<leader>sc", display.close_all, {})
-		keymap.set("v", "<leader>sr", ":'<,'>SnipRun<CR>")
+		sniprun.setup(opts)
 	end,
+	keys = {
+		{
+			"<leader>sc",
+			function()
+				require("sniprun.display").close_all()
+			end,
+			desc = "Close",
+		},
+		{
+			"<leader>sr",
+			function()
+				require("sniprun").run()
+			end,
+			desc = "Current",
+		},
+		{
+			"<leader>si",
+			function()
+				require("sniprun").info()
+			end,
+			desc = "Info",
+		},
+		{
+			"<leader>sl",
+			function()
+				require("sniprun.live_mode").toggle()
+			end,
+			desc = "Live Mode",
+		},
+		{
+			"<leader>sp",
+			function()
+				require("sniprun").clear_repl()
+			end,
+			desc = "Clear REPL",
+		},
+		{
+			"<leader>rs",
+			function()
+				require("sniprun").reset()
+			end,
+			desc = "Reset",
+		},
+		{
+			"<leader>sr",
+			function()
+				require("sniprun").run("v")
+			end,
+			mode = { "v" },
+			desc = "Selection",
+		},
+	},
 }
