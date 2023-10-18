60f6d666c36a4a3742435205b9766fd4a458d371
van <liufan421@gmail.com>
2023-10-18 09:25:16 +0800

    add usage for comment.nvim

--- lua/van/plugins/comment.lua
+++ lua/van/plugins/comment.lua
@@ -5,4 +5,19 @@ return {
 		"BufNewFile",
 	},
 	config = true,
+	-- Usage
+	--[[ NORMAL mode ]]
+	-- `gcc` - Toggles the current line using linewise comment
+	-- `gbc` - Toggles the current line using blockwise comment
+	-- `[count]gcc` - Toggles the number of line given as a prefix-count using linewise
+	-- `[count]gbc` - Toggles the number of line given as a prefix-count using blockwise
+	-- `gc[count]{motion}` - (Op-pending) Toggles the region using linewise comment
+	-- `gb[count]{motion}` - (Op-pending) Toggles the region using blockwise comment
+	--[[ VISUAL mode ]]
+	-- `gc` - Toggles the region using linewise comment
+	-- `gb` - Toggles the region using blockwise comment
+	--[[ NORMAL mode ]]
+	-- `gco` - Insert comment to the next line and enters INSERT mode
+	-- `gcO` - Insert comment to the previous line and enters INSERT mode
+	-- `gcA` - Insert comment to end of the current line and enters INSERT mode
 }
